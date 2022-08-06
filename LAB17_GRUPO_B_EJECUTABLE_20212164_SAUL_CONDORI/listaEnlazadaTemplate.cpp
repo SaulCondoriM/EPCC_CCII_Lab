a31d7706d228060527daf2ea022613ebd162d89f
SaulCondoriM <104391550+SaulCondoriM@users.noreply.github.com>
2022-08-06 13:38:05 -0500

    Add files via upload

---
+++ LAB17_GRUPO_B_EJECUTABLE_20212164_SAUL_CONDORI/listaEnlazadaTemplate.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+template <typename T>
+struct Nodo{
+	
+	T valor;
+	Nodo* siguiente;
+};
+
+template <class T>
+class ListaEnlazada{
+private:
+	Nodo<T>* cabeza;
+public:
+	ListaEnlazada(){ 
+		cabeza = NULL; 
+	}
+	void Insertar(T d);
+	void Mostrar();
+};
+
+template <class T>
+void ListaEnlazada<T>::Insertar(T d)
+{
+	Nodo<T> *listEnlaz = new Nodo<T>;
+	listEnlaz->valor = d;
+	
+	listEnlaz->siguiente = cabeza;
+	cabeza = listEnlaz;
+}
+
+template <class T>
+void ListaEnlazada<T>::Mostrar(){
+	
+	Nodo<T>* listaActual = cabeza;
+	while( listaActual != NULL ){
+		
+		cout << listaActual->valor << " -> ";
+		listaActual = listaActual->siguiente;
+	}
+}
+int main(int argc, char *argv[]) {
+	
+	cout<<"\nLISTA ENLAZADA DE 100 NUMEROS ALEATORIOS"<<endl;
+	
+	int arrNum[100] ;
+	srand(time(0));
+	
+	//GENERANDO NUMEROS ALEATORIOS
+	
+	for(int i = 0; i < 100; i++){
+		arrNum[i] = rand() % 101;
+	}
+	
+	ListaEnlazada<int> numeros;
+	
+	for(int i = 0; i < 100; i++){
+		numeros.Insertar(arrNum[i]);
+	}
+	
+	numeros.Mostrar();
+	
+	cout<<endl;
+	cout<<"\nLISTA ENLAZADA DE 100 LETRAS ALEATORIAS"<<endl;
+	
+	int arrLetr[100] ;
+	
+	
+	//GENERANDO LETRAS ALEATOREAS
+	
+	for(int i = 0; i < 100; i++){
+		arrLetr[i] = 'a' + rand() % 26;
+	}
+	
+	
+	ListaEnlazada<char> letras;
+	
+	for(int i = 0; i < 100; i++){
+		letras.Insertar(arrLetr[i]);
+	}
+	
+	
+	letras.Mostrar();
+	
+	return 0;
+}
+
---
+++ LAB17_GRUPO_B_EJECUTABLE_20212164_SAUL_CONDORI/operacionesTemplates.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+
+template <typename T, typename U>
+
+class OperacionesBasicas{
+private:
+	T x;
+	U y;
+public:
+	OperacionesBasicas(T x, U y);
+	
+	U Suma(){
+		return x + y;
+	};
+		
+	U Resta(){
+		return x - y;
+	};
+	U Multiplicacion(){
+		return x * y;
+	};
+	U Division(){
+		return x / y;
+	}
+	
+};
+
+template <typename T, typename U>
+OperacionesBasicas <T,U> :: OperacionesBasicas(T x, U y){
+	this->x = x;
+	this->y = y;
+}
+
+int main(int argc, char *argv[]) {
+	
+	OperacionesBasicas<int, float> Calculadora (18, 3.1);
+	
+	cout<<"\nSUMA"<<endl;
+	cout<<Calculadora.Suma()<<endl;
+	
+	cout<<"\nRESTA"<<endl;
+	cout<<Calculadora.Resta()<<endl;
+	
+	cout<<"\nMULTIPLICACION"<<endl;
+	cout<<Calculadora.Multiplicacion()<<endl;
+	
+	cout<<"\nDIVISION"<<endl;
+	cout<<Calculadora.Division()<<endl;
+	
+	
+	
+	return 0;
+}
+
